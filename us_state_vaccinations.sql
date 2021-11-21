1907c138f940f7e14db569daaec183fbfd44bd06
Luis <90655348+datarobaticsmex@users.noreply.github.com>
2021-11-21 12:52:37 -0700

    Update us_state_vaccinations.sql

--- us_state_vaccinations.sql
+++ us_state_vaccinations.sql
@@ -13,9 +13,15 @@ From PortfolioProject..us_state_vaccinations$
 --Where location like '%alabama%'
 Group by Date, Location
 
+
 Select Date, Location, Total_Vaccinations, Total_Distributed, MAX(People_vaccinated) as Highest_People_Vaccinated
 From PortfolioProject..us_state_vaccinations$
 Group by Date, Location, Total_Vaccinations, Total_distributed, People_vaccinated
+
+
+Select date, location, total_vaccinations, total_distributed, people_fully_vaccinated
+From PortfolioProject..us_state_vaccinations$
+Order by total_vaccinations DESC
 
 
 
